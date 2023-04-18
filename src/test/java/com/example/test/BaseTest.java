6dc95b800288e438fd53866bf359ee4547fbd7ba
Monica Hoyos <moni.hoyosg@gmail.com>
2023-04-18 17:45:12 -0500

    Ass src test classes

---
+++ src/test/java/com/example/test/BaseTest.java
@@ -0,0 +1,39 @@
+package com.example.test;
+
+import com.example.pages.BuscadorPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    public WebDriver driver;
+    public BuscadorPage buscadorPage;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeMethod
+    public void inicializar() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--start-maximized");
+        //options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        buscadorPage = new BuscadorPage(driver);
+    }
+
+    @AfterMethod
+    public void tearDown () {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
---
+++ src/test/java/com/example/test/Launch.java
@@ -0,0 +1,44 @@
+package com.example.test;
+
+import com.example.test.utils.Listeners.TestListener;
+import io.qameta.allure.*;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+@Listeners({ TestListener.class })
+@Epic("Allure TestNG Suite Tests")
+@Feature("Allure TestNG Tests")
+public class Launch extends BaseTest {
+
+    public Properties fileprops = new Properties();
+
+    public Properties getProperties() throws Exception {
+        fileprops.load(new FileInputStream(new File("src/test/resources/test.properties").getAbsolutePath()));
+        return fileprops;
+    }
+
+    @Test(priority=0, description="Realizar una búsqueda con resultados")
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Realizar una búsqueda con resultados")
+    @Story("Busqueda")
+    @TmsLink("XRPRJ-1")
+    public void test_busqueda_con_datos () throws Exception {
+        buscadorPage.gotoBuscador(getProperties().getProperty("url"))
+                    .searchText(getProperties().getProperty("textobusqueda1"))
+                    .comprobarResultadoCorrecto();
+    }
+
+    @Test(priority=1, description="Realizar una búsqueda sin resultados")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Realizar una búsqueda sin resultados")
+    @Story("Busqueda")
+    @TmsLink("XRPRJ-3")
+    public void test_busqueda_sin_datos () throws Exception {
+        buscadorPage.gotoBuscador(getProperties().getProperty("url"))
+                     .searchText(getProperties().getProperty("textobusqueda2"))
+                     .comprobarResultadoIncorrecto();
+    }
+}
